99c62825d1b9c667802f2c453ff1fd32bb336500
Nitin Sharma <nitindepp2005@gmail.com>
2024-06-13 20:42:24 +0530

    .

--- Data Structure/2D Array/Spiral_Order_Matrix_Traversal.cpp
+++ Data Structure/2D Array/Spiral_Order_Matrix_Traversal.cpp
@@ -1,3 +1,5 @@
+/* 5 6
+1 5 7 9 10 11 6 10 12 13 20 21 9 25 29 30 32 41 15 55 59 63 68 70 40 70 79 81 95 105 */
 #include <iostream>
 using namespace std;
 
@@ -16,14 +18,6 @@ int main()
             cin >> array[i][j];
         }
     }
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            cout << array[i][j] << " ";
-        }
-        cout << endl;
-    }
 
-    return 0;
+        return 0;
 }
